677e86f11cfef027670b37b2aa2a0ad63ee34ad5
marcelo_victor <marcelo.victor525@gmail.com>
2023-01-29 00:01:20 -0300

    Adicionando tratamento de erros

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -19,6 +19,10 @@ async function criaVideo(titulo, descricao, url, imagem) {
         })
     });
 
+    if (!conexao.ok) {
+        throw new Error("Não foi possivel encontrar o video");
+    }
+
     const conexaoConvertida = await conexao.json();
     return conexaoConvertida;
 }
